10816a42c5bcdff9cd297ae656489fd821530eb2
Vishal-Bysani <vishal.bysani99@gmail.com>
2023-07-20 11:10:55 +0530

    Uploaded the folder for counter app

---
+++ counter_app/lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(CounterApp());
+}
+
+class CounterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Counter',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: CounterPage(),
+    );
+  }
+}
+
+class CounterPage extends StatefulWidget {
+  @override
+  _CounterPageState createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+  void _decrementCounter(){
+    setState((){
+      _counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Counter App'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Counter Value:',style: TextStyle(fontSize: 20),),
+              Text('$_counter',style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+
+        floatingActionButton: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              FloatingActionButton(
+                child: Icon( Icons.add),
+                onPressed: _incrementCounter,
+                tooltip: 'Increment',
+              ),
+
+              SizedBox(width: 10,),
+
+              FloatingActionButton(
+
+                onPressed: _decrementCounter,
+                tooltip: 'Decrement',
+                child: Icon(Icons.remove),
+              ),
+
+            ]
+        )
+
+    );
+  }
+}
